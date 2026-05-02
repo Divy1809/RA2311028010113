db4177d26047ea0761bf5f68a1be6feab82f755f
Divy Jain <157494165+Divy1809@users.noreply.github.com>
2026-05-02 11:19:38 +0530

    feat: implemented priority inbox using heap-based top N logic

--- notification_app_fe/src/pages/PriorityInbox.tsx
+++ notification_app_fe/src/pages/PriorityInbox.tsx
@@ -1,5 +1,19 @@
+import NotificationCard from "../components/NotificationCard";
+import { mockNotifications } from "../data/mockNotifications";
+import { getTopNotificationsHeap } from "../utils/getTopNotificationsHeap";
+
 function PriorityInbox() {
-  return <div>Priority Inbox Page</div>;
+  const topNotifications = getTopNotificationsHeap(mockNotifications, 10);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Priority Inbox (Top 10)</h2>
+
+      {topNotifications.map((notif) => (
+        <NotificationCard key={notif.ID} notification={notif} />
+      ))}
+    </div>
+  );
 }
 
 export default PriorityInbox;
